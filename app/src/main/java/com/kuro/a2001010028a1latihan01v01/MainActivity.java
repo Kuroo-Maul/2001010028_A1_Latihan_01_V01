3d5f073539909e52c42729c5e6a5ee386bb2ce94
Kuro-Maul <gm1322878@gmail.com>
2021-10-19 18:19:24 +0800

    Initial commit

---
+++ app/src/main/java/com/kuro/a2001010028a1latihan01v01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.kuro.a2001010028a1latihan01v01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+public void Main(View view){
+
+        setContentView(R.layout.latihan_01);
+}
+
+}
